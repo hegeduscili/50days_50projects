c1dce206f253fa2240be20b46b835ed4cca51400
hegeduscili <hegeduscecilia@gmail.com>
2023-10-06 14:38:10 +0200

    2023.10.06

---
+++ blury_backgroung/scripts2.js
@@ -0,0 +1,28 @@
+const bg = document.querySelector('.bg')
+const loadingtext = document.querySelector('.loading-text')
+
+
+let load = 0;
+
+let int = setInterval(blurring, 30)
+
+
+function blurring() {
+
+    load++
+
+    if(load > 99){
+        clearInterval(int)
+
+    }
+
+    loadingtext.innerHTML = `${load}%`
+    loadingtext.style.opacity = scale (load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale (load, 0, 100, 30, 0)}px)`;
+  
+    console.log(load);
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) =>{
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min 
+   }
